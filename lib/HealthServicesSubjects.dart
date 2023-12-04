c25a049b3937d283551a5919e1fc1b0f743aab29
jaegc <jgcoll06@louisville.edu>
2023-12-04 12:42:26 -0500

    Health Services Fixed (Carter)

--- lib/HealthServicesSubjects.dart
+++ lib/HealthServicesSubjects.dart
@@ -3,18 +3,25 @@ import 'package:flutter/material.dart';
 class HealthServicesSubjects extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SubjectCard(title: 'Campus Safety', content: 'Content for Campus Safety page goes here.'),
-        SubjectCard(title: 'Concern Center', content: 'Content for Concern Center page goes here.'),
-        SubjectCard(title: 'Counseling Center', content: 'Content for Counseling Center page goes here.'),
-        SubjectCard(title: 'Emergency Text Alerts', content: 'Content for Emergency Text Alerts page goes here.'),
-        SubjectCard(title: 'Emergency Procedures', content: 'Content for Emergency Procedures page goes here.'),
-        SubjectCard(title: 'Escort Service', content: 'Content for Escort Service page goes here.'),
-        SubjectCard(title: 'HSC Counseling Services', content: 'Content for HSC Counseling Services page goes here.'),
-        SubjectCard(title: 'Student Health Services', content: 'Content for Student Health Services page goes here.'),
-        SubjectCard(title: 'ULPD', content: 'Content for ULPD page goes here.'),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Health Services'),
+        backgroundColor: Colors.black,
+      ),
+      body: ListView(
+          padding: const EdgeInsets.all(8),
+        children: [
+          SubjectCard(title: 'Campus Safety', content: 'Campus Safety: CardSafe is a program to help protect all members of our campus community. We encourage you to read this website and take advantage of all the safety measures and programs listed. Of course, no one can absolutely guarantee your safety. But by utilizing these programs and using some common sense, you can greatly reduce your odds of becoming a victim on or near any of our three campuses.L Trail / Green Dot.'),
+          SubjectCard(title: 'Concern Center', content: 'ConcernCenter: https://louisville.concerncenter.com/'),
+          SubjectCard(title: 'Counseling Center', content: 'Counseling Center: If you are in crisis or need to speak with someone urgently please call us at 852-6585 M-F or walk-in between 9am and 4pm (when the center is open) for an urgent consultation, and we will assign a clinician to speak with you as soon as possible. We do not schedule appointments for UCs, so please call or walk in when you are ready to speak with a clinician. Please note that urgent consultation (UC) services are provided on a first come, first served basis. Our busiest time is 2-4pm. You will need time to complete paperwork and see or receive a callback from a clinician, and we do not schedule appointments for UCs. We may have to call you back/see you on the following day if there are several students ahead of you. Beginning in Fall 2021, all services will be offered via telehealth and/or in-person due to the COVID-19 pandemic. The Counseling Center will follow CDC guidelines and University policy to ensure the safety of all students and staff. For after hours or emergency services For self-care, coping, and wellness resources, see our Coping with COVID-19 section For information on local agencies offering therapy services, see our Community Resources section Resources and Self-Care Tips In case of a life-threatening emergency (including thoughts of hurting yourself or someone else), call: 911 University Police (ULPD) (502) 852-6111 Emergency Psychiatry at the University of Louisville Hospital (502) 562-3120 24/7 Adult Crisis Line (502) 589-4313 or 800-221-0446 Suicide Prevention Hotline call or text 988 OR 800-273-TALK (8255)'),
+          SubjectCard(title: 'Emergency Text Alerts', content: 'Emergency Text Alerts: University Alert System UofL wants to protect its greatest asset: YOU. During an emergency or when campus conditions affect class/work schedules, you will receive information: On your phone By text message to a phone number of your choice. Ability to add additional phone numbers. Broadcast and/or written message to all office, classroom and dorm Cisco IP phones. Via email By e-mail message to your official UofL e-mail address Ability to add additional e-mail addresses On the web On the universitys homepage'),
+          SubjectCard(title: 'Emergency Procedures', content: 'Emergency Procedures: Emergency Management Located within the University of Louisville Police Department and Office of Public Safety, the Emergency Management Program leads the coordination of campus, local, state, federal, and other community resources to provide comprehensive emergency management operations to the University of Louisville community. Emergency Management oversees the prevention, mitigation, preparedness, response, and recovery activities that protect life, property, and the environment from all man-made and natural threats and disasters that impact the campus community and its neighbors. If you are looking for documents or information not available on this page, contact: Mindy Christian Manager, Emergency Management Office: 502-852-9099, Cell: 502-779-1466 Jennifer Twagira Facilities Coordinator Sr. Office: 502-852-1113 Emergency - Call 911 Non-Life Threathing Emergency - Call 502-852-6111'),
+          SubjectCard(title: 'Escort Service', content: 'Escort Service: https://louisville.edu/police/services'),
+          SubjectCard(title: 'HSC Counseling Services', content: 'HSC Counseling Services: Counseling Services Counseling Services are available at no charge to currently enrolled students at the Belknap Counseling Center and the Health Sciences Campus (HSC) Counseling Office. Belknap Counseling Center Although not part of Campus Health Services, the Belknap Counseling Center works closely with our primary care and psychiatric providers. For more information regarding the Belknap Counseling Center please click here. Health Sciences Campus Counseling Office The Health Sciences Center Counseling Office offers short-term individual counseling, group counseling, and psychological assessment. Students seek support through counseling for a variety of reasons, including psychological, personal/social, academic, and/or career concerns. If you are experiencing any of these concerns and/or would like to connect with a counselor, please call Campus Health Services at 502-852-6446 to set up an appointment. These services are offered to students in the following schools and programs: School of Medicine, School of Dentistry, School of Nursing, School of Public Health and Informational Sciences Resident Physicians and Dental Fellows IPIBS/Graduate students on the HSC Campus, including post-doctorate students who are not classified as employees under U of L Human Resources. Students in the audiology program and the speech and language pathology program The HSC Counseling Office also provides consultation with faculty and staff regarding student concerns, outreach, training, and advocacy to the University of Louisville community. Important Details Services are FREE for all University of Louisville HSC students. Access to service is VOLUNTARY. Some administration policies will allow faculty and staff to recommend strongly that a student meet with an HSC counselor or physician, but each student can discuss with a deans representative his or her choice to refuse the referral. Confidentiality We respect students privacy, so all communication within the HSC Counseling Office is confidential, with a few exceptions. These exceptions are: when the information relates to clear and imminent danger to an individual, when a state law requires the reporting of information (threats of violence, child abuse or neglect, spouse abuse), and when information is requested by court order. Any disclosure in these situations will be made to an appropriate authority and will be limited to material directly pertinent to the issue involved. Please note that we will not release any information obtained through interviews, counseling sessions, or testing without the written permission of a client. IF YOU ARE EXPERIENCING A LIFE-THREATENING EMERGENCY, please contact the following: Emergency Psychiatric Services (EPS) - 502-562-3120 Caller should identify self as associated with U of L and ask to speak to a clinician. Follow any instructions exactly. 911: Follow any instructions exactly. See https://louisville.edu/counseling/emergencies or http://louisville.edu/campushealth/emergencies/mental-health for further information.'),
+          SubjectCard(title: 'Student Health Services', content: 'Student Health Services: https://louisville.edu/campushealth'),
+          SubjectCard(title: 'ULPD', content: 'University of Louisville Police Department (ULPD): The mission of the Department of Public Safety and the University of Louisville Police Department is to maintain public peace and safety, safeguard the assets of the institution and our faculty, staff, students and visitors, and assist in providing an environment conducive to the teaching, research and public service missions of the University. ULPD serves all eligible faculty, staff, and students regardless of race, color, national origin, sex, disability, or age. Belknap Campus Floyd Street Parking Garage 2126 South Floyd Street,Suite 100 Louisville, KY 40292 (502) 852-6111 Health Sciences Center Campus Abell Administration Building 323 E. Chestnut Street,Suite 120 Louisville, KY 40202 (502) 852-2402 Shelby CampusFounders Union Building450 N. Whittington Pkwy,Suite 136A Louisville, KY 40222(502) 852-1436Link to Anonymous University Reporting form Anonymous University Reporting UofL uses a Maxient system, the nations premier public safety incident reporting software for universities and colleges, for their anonymous information reporting form. Notes: Information only required for fields with a red asterisk. All forms reviewed prior to receipt by ULPD.Link to Bicycle Registration formProtect Your Bike on Campus Parking Services asks that you register your bike for use on each campus. Remember to always lock your bicycle to a designated rack with a secure lock. Rent a "U" lock from SGA through the FREE bike lock rental program. While riding on campus, be diligent about avoiding pedestrians and obey all traffic laws. Link to sign up for University AlertsSign Up for University Alerts We recommend that all students, faculty and staff sign up for the UofL Alert System for receiving text or email messages in case of an emergency on campus. Our text messaging service sends the most up-to-date information direct to your mobile device, including all weather cancellations. Link to download Rave Guardian AppDownload our Rave Guardian App Rave Guardian is the nations leading smart phone app for personal safety. Download this free app, customized for use by the University of Louisville, on your Apple or Android device to enhance safety on campus through interactive features and a virtual network of friends, family and Campus Police.'),
+        ],
+      ),
     );
   }
 }
@@ -88,13 +95,19 @@ class SubjectDetailPage extends StatelessWidget {
             ),
             SizedBox(height: 16.0),
             Text(content),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context); // This will navigate back to the previous page
+              },
+              child: Text('Go Back'),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
 /*
   String currentPage = 'Health Resources'; // Default page
 
